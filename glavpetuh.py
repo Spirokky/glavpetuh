c7c52838bac679bf34fae044db6dc93e1251efa9
Spirokky <spirokky@yandex.ru>
2017-04-01 12:04:20 +0300

    proxy for PythonAnywhere

--- glavpetuh.py
+++ glavpetuh.py
@@ -3,8 +3,17 @@ import telepot
 import sys
 import time
 import l2onparser
+import urllib3
 
 from pprint import pprint
+
+# ------ Magic for PythonAnywhere free account ------ 
+# proxy_url = "http://proxy.server:3128"
+# telepot.api._pools = {
+#     'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
+# }
+# telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
+# ------ end of magic ------
 
 TOKEN = sys.argv[1]
 TRUSTED = [-188672102, # testdebug
