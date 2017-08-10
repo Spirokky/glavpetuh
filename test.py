589b9ab669a78c129eb73ca5523a01e9ccd1811f
Spirokky <spirokky@yandex.ru>
2017-08-10 18:45:14 +0300

    setUp and tearDown updated to clean DB after tests run

--- test.py
+++ test.py
@@ -8,18 +8,20 @@ class TestQuotes(unittest.TestCase):
     def setUp(self):
         self.q = Quote('core/testing_database.db')
 
+        with self.q.connect:
+            self.q.cursor.execute("SELECT id FROM quotes WHERE id = (SELECT MAX(id)  FROM quotes);")
+            self.last_row = self.q.cursor.fetchone()
+
     def tearDown(self):
-        self.q.connect.rollback()
-        self.q.connect.close()
+        with self.q.connect:
+            self.q.cursor.execute("DELETE FROM quotes WHERE id > (?);", self.last_row)
 
     def test_get(self):
         self.assertTupleEqual(self.q.get(3), (3, 'Все дороги ведут к людям.'))
         self.assertIsNotNone(self.q.get(1))
 
     def test_get_with_no_id(self):
-        with self.assertRaises(ValueError):
-            self.q.get(False)
-            self.q.get()
+        self.assertTrue(type(self.q.get()), tuple)
 
     def test_get_with_str(self):
         self.assertTupleEqual(self.q.get("3"), (3, 'Все дороги ведут к людям.'))
@@ -33,6 +35,18 @@ class TestQuotes(unittest.TestCase):
     def test_getrandom_with_args(self):
         with self.assertRaises(TypeError):
             self.q.getrandom(123)
+
+    def test_add(self):
+        self.assertTrue(type(self.q.add('Жить дальше – безнравственно')), tuple)
+        self.assertEqual(self.q.add('Бармен все равно что психотерапевт.')[1], ('Бармен все равно что психотерапевт.'))
+
+    def test_add_no_args(self):
+        with self.assertRaises(TypeError):
+            self.q.add()
+            self.q.add(False)
+
+    def test_remove(self):
+        pass
 
 
 if __name__ == "__main__":
