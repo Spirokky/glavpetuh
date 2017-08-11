6d5be2193b7d621509ffd6445154f746a5982dbc
Spirokky <spirokky@yandex.ru>
2017-08-11 18:09:57 +0300

    LIST_OF_ACCESS now located in config.py

--- bot.py
+++ bot.py
@@ -11,14 +11,12 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-LIST_OF_ACCESS = [303422193]
-
 
 def restricted(func):
     @wraps(func)
     def wrapped(bot, update, *args, **kwargs):
         user_id = update.effective_user.id
-        if user_id not in LIST_OF_ACCESS:
+        if user_id not in config.LIST_OF_ACCESS:
             print("Unauthorized access denied for {}.".format(user_id))
             update.message.reply_text('Недостаточно прав.')
             return
