7f221b9deaa09bdb32711e41e806abe0ad68478e
Spirokky <spirokky@yandex.ru>
2017-09-02 14:55:09 +0300

    vote command added to help message

--- src/config/config.py
+++ src/config/config.py
@@ -7,6 +7,9 @@ help_msg = """
 */lvl* `lvl` `%` - показывает сколько опыта осталось до lvl-up
 */exp* `от` `до` - таблица опыта в выбранном диапазоне лвл-ов.
 
+*[Разное]*
+*/vote* `текст` - голосование Да/Нет
+
 *[L2on]*
 */*`nickname` - информация о персонаже с [l2on.net](http://l2on.net)
 
@@ -24,8 +27,8 @@ LIST_OF_GROUPS = [-1001105947437,  # back to l2
                   303422193,       # Spirokky's private
                  ]
 
-#webdriver_path = "D:\Downloads\phantomjs-2.1.1-windows\\bin\phantomjs.exe"
-webdriver_path = "phantomjs-2.1.1-linux-x86_64/bin/phantomjs"
+webdriver_path = "D:\Downloads\phantomjs-2.1.1-windows\\bin\phantomjs.exe"
+# webdriver_path = "phantomjs-2.1.1-linux-x86_64/bin/phantomjs"
 
 levels = {
     '1': (0, 0),
