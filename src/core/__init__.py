e1ee5c0bb11c5fff62590cb61e54b1a6a7dc25e8
Spirokky <spirokky@yandex.ru>
2017-10-27 15:12:51 +0300

    utils added

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,3 +1,4 @@
 from .exp import Exp, render_mpl_table
+from .utils import clean_data, validate_nickname
 
-__all__ = ['Exp', 'render_mpl_table']
+__all__ = ['Exp', 'render_mpl_table', 'clean_data', 'validate_nickname']
