0eeb1f2c007a34c8b6882929447f9677f458b3ed
Spirokky <spirokky@yandex.ru>
2017-10-27 16:01:40 +0300

    l2on import added

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,4 +1,6 @@
 from .exp import Exp, render_mpl_table
+from .l2on import Player
 from .utils import clean_data, validate_nickname
 
-__all__ = ['Exp', 'render_mpl_table', 'clean_data', 'validate_nickname']
+__all__ = ['Exp', 'Player', 'render_mpl_table', 'clean_data',
+           'validate_nickname']
