b9d26b98c18a482a8a5b200c0bf7c14306db6fd5
Spirokky <spirokky@yandex.ru>
2017-11-01 16:54:55 +0300

    models initializing, Quote and Tweet models added

---
+++ src/core/models.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from peewee import *
+from playhouse.migrate import migrate, SqliteMigrator
+
+
+db = SqliteDatabase('data_test.db', timeout=10)
+
+
+class Quote(Model):
+    text = TextField()
+    created_date = DateTimeField(default=datetime.now)
+    last_access = DateTimeField(default=datetime.now)
+
+    class Meta:
+        database = db
+
+
+class Tweet(Model):
+    tweet_id = BigIntegerField(unique=True)
+    text = TextField()
+    created_date = DateTimeField()
+    known_at = DateTimeField(default=datetime.now)
+    photo_url = TextField(default='')
+
+    class Meta:
+        database = db
+
+
+for t in (Quote, Tweet):
+    t.create_table(fail_silently=True)
+
+
+migrator = SqliteMigrator(db)
+
+operations = [
+    migrator.add_column('quote', 'last_access', Quote.last_access),
+]
+
+for op in operations:
+    try:
+        migrate(op)
+    except OperationalError:
+        pass
