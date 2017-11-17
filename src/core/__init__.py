42c63fb51bd26e9a3c3b70cf352fe6b7ca38cc7c
Spirokky <spirokky@yandex.ru>
2017-11-17 12:15:53 +0300

    render_mpl_table moved to utils

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,7 +1,7 @@
 from .models import Quote, Tweet
-from .exp import Exp, render_mpl_table
+from .exp import Exp
 from .l2on import Player
-from .utils import clean_data, validate_nickname
+from .utils import clean_data, validate_nickname, render_mpl_table
 
 
 __all__ = ['Quote', 'Tweet', 'Exp', 'Player', 'render_mpl_table', 'clean_data',
--- src/core/utils.py
+++ src/core/utils.py
@@ -1,5 +1,12 @@
 import re
 import string
+import numpy as np
+import six
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+from datetime import datetime
 
 
 def clean_data(dct):
@@ -20,6 +27,39 @@ def validate_nickname(nickname):
     else:
         return 3
 
+
+def render_mpl_table(data, col_width=1.0, row_height=0.625, font_size=12,
+                     header_color='#40466e', row_colors=['#f7f7f7', 'w'],
+                     edge_color='w', bbox=[0, 0, 1, 1], header_columns=0,
+                     ax=None, **kwargs):
+
+    if ax is None:
+        size = (np.array(data.shape[::-1]) + np.array([0, 1])) * np.array([col_width, row_height])
+        fig, ax = plt.subplots(figsize=size)
+        fig.subplots_adjust(left=0, bottom=0, right=1, top=1)
+        ax.axis('off')
+
+    mpl_table = ax.table(cellText=data.values, cellLoc="center", bbox=bbox, colLabels=data.columns, **kwargs)
+
+    mpl_table.auto_set_font_size(False)
+    mpl_table.set_fontsize(font_size)
+
+    for k, cell in six.iteritems(mpl_table._cells):
+        cell.set_edgecolor(edge_color)
+        if k[0] == 0 or k[1] < header_columns:
+            cell.set_text_props(weight='bold', color='w')
+            cell.set_facecolor(header_color)
+        else:
+            cell.set_facecolor(row_colors[k[0]%len(row_colors)])
+
+    try:
+        now = datetime.now().strftime("%Y-%m-%d")
+        filename = "{}-stats.png".format(now)
+        plt.savefig(filename)
+        return filename
+    except Exception:
+        return None
+    
 
 if __name__ == '__main__':
     pass
